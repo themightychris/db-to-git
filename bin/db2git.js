1e3559938e3f91ef355a616c51bb913e81b806a7
timwis <tim@timwis.com>
2016-11-22 20:10:55 -0500

    begin import

--- bin/db2git.js
+++ bin/db2git.js
@@ -2,13 +2,16 @@ require('dotenv').config({ silent: true })
 const meow = require('meow')
 
 const exportTable = require('../lib/export')
+const importTable = require('../lib/import')
 
 const help = `
   Usage
     $ db2git export <table> [--key=id] [--output=./<table>]
+    $ db2git import <directory> [--key=id] [--table=<directory>]
 
   Options
     -o --output <directory>    Path to output directory [default: (table name)]
+    -t --table <table>         Table name to import into [default: (directory name)]
     -k --key <primarykey>      Unique column to use as directory names [default: id]
 
   Examples:
@@ -17,6 +20,7 @@ const help = `
 const cli = meow(help, {
   alias: {
     o: 'output',
+    t: 'table',
     k: 'key'
   },
   default: {
@@ -25,15 +29,17 @@ const cli = meow(help, {
 })
 
 const command = cli.input.length && cli.input[0]
-const table = cli.input.length && cli.input[1]
+const argument = cli.input.length && cli.input[1]
 
 const opts = Object.assign({}, cli.flags, {
   client: process.env.DB_CLIENT,
   connection: process.env.DB_CONNECTION
 })
 
-if (command === 'export' && table) {
-  exportTable(table, opts)
+if (command === 'export' && argument) {
+  exportTable(argument, opts)
+} else if (command === 'import' && argument) {
+  importTable(argument, opts)
 } else {
   cli.showHelp()
 }
